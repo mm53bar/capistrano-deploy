cf4d76d96ae7d260db195439d1b13eedc8b7710f
Michael van den Beuken <michael.beuken@ama.ab.ca>
2013-02-26 12:09:37 -0700

    set the appropriate tabbing for a yml file. removed hard-coded environment

--- lib/capistrano-deploy/database.rb
+++ lib/capistrano-deploy/database.rb
@@ -6,15 +6,15 @@ module CapistranoDeploy
           desc 'Generate database.yml'
           task :configure, :roles => :app, :except => {:no_release => true} do
             database_yml = <<-EOF
-            production:
-              adapter: #{ENV['DB_ADAPTER']}
-              host: #{ENV['DB_HOST']}
-              port: #{ENV['DB_PORT']}
-              encoding: #{ENV['DB_ENCODING']}
-              database: #{ENV['DB_NAME']}
-              pool: #{ENV['DB_POOL']}
-              username: #{ENV['DB_USER']}
-              password: #{ENV['DB_PASSWORD']}
+#{ENV['DB_ENV']}:
+  adapter: #{ENV['DB_ADAPTER']}
+  host: #{ENV['DB_HOST']}
+  port: #{ENV['DB_PORT']}
+  encoding: #{ENV['DB_ENCODING']}
+  database: #{ENV['DB_NAME']}
+  pool: #{ENV['DB_POOL']}
+  username: #{ENV['DB_USER']}
+  password: #{ENV['DB_PASSWORD']}
             EOF
 
             put database_yml, "#{deploy_to}/config/database.yml"
