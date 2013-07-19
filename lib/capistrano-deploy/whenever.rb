d12a18bfa3c6b00f2e659ee5eebb79a8cace622c
Jordan Babe <jordan.babe@ama.ab.ca>
2013-07-19 13:12:31 -0600

    So whenever doesn't default to :db role

--- lib/capistrano-deploy/whenever.rb
+++ lib/capistrano-deploy/whenever.rb
@@ -18,14 +18,16 @@ module CapistranoDeploy
           end
         end
 
+        set(:whenever_role) { :db }
+
         namespace :whenever do
           desc 'Update crontab file'
-          task :update_crontab, :roles => :db, :only => {:primary => true} do
+          task :update_crontab, :roles => :whenever_role, :only => {:primary => true} do
             run "cd #{deploy_to} && #{whenever_cmd} --update-crontab #{whenever_identifier}"
           end
 
           desc 'Clear crontab file'
-          task :clear_crontab, :roles => :db, :only => {:primary => true} do
+          task :clear_crontab, :roles => :whenever_role, :only => {:primary => true} do
             run "cd #{deploy_to} && #{whenever_cmd} --clear-crontab #{whenever_identifier}"
           end
         end
