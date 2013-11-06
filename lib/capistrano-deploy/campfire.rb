6d28f5b08371808c87d76d5d105085a1ebc63736
Darko Dosenovic <darko.dosenovic@ama.ab.ca>
2013-11-06 16:13:11 -0700

    Adjusting code after review.

--- lib/capistrano-deploy/campfire.rb
+++ lib/capistrano-deploy/campfire.rb
@@ -4,16 +4,16 @@ module CapistranoDeploy
       configuration.load do
         namespace :campfire do
           require 'campy'
-          env = Dotenv.load '.env'
-          campy = Campy::Room.new(account: env['CAMPFIRE_ACCOUNT'], token: env['CAMPFIRE_TOKEN'], room_id: env['CAMPFIRE_ROOM_ID'])
 
           desc 'Set notification in campfire for deployment start'
           task :start_msg do
-            campy.speak "#{`whoami`.upcase} is starting deploy of '#{app_name.upcase}' from branch '#{branch}' to #{current_stage.upcase}"
+            campy = Campy::Room.new(account: campfire_account, token: campfire_token, room_id: campfire_room_id)
+            campy.speak "#{campfire_speaker.upcase} is starting deploy of '#{app_name.upcase}' from branch '#{branch}' to #{current_stage.upcase}"
           end
 
           desc 'Set notification in campfire for deployment end'
           task :end_msg do
+            campy = Campy::Room.new(account: campfire_account, token: campfire_token, room_id: campfire_room_id)
             campy.speak "Deployment of '#{app_name.upcase}' from branch '#{branch}' to #{current_stage.upcase} was successful!"
             campy.play "pushit"
           end
