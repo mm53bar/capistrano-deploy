3985d7b67cd71ef2af005fdaab87ccc896aeb1c0
Jordan Babe <jordan.babe@ama.ab.ca>
2014-01-03 15:24:05 -0700

    Check if config files have changed
    * If .env hasn't changed and user hasn't explicitly
    said they want to replace it - don't.
    * Also let the user set the name of the .env file. This
    is something I'm using for testing (so I don't blow away
    an existing .env with a test .env

--- lib/capistrano-deploy/environment.rb
+++ lib/capistrano-deploy/environment.rb
@@ -3,17 +3,20 @@ module CapistranoDeploy
     def self.load_into(configuration)
       configuration.load do
 
+        set(:environment_file) { ".env" }
+
         namespace :environment do
           desc "Copy ENV file from remote git repository to your staging/production servers"
           task :fetch_config, :roles => :app, :except => {:no_release => true} do
             path_to_new_env = File.join deploy_to, "tmp", "env_config", app_name, ".env.#{current_stage}"
-            path_to_old_env = File.join deploy_to, ".env.fake"
+            path_to_old_env = File.join deploy_to, environment_file
 
             run "rm -rf #{deploy_to}/tmp/env_config && git clone -n #{environment_repository} --depth 1 #{deploy_to}/tmp/env_config"
-            run "cd #{deploy_to}/tmp/env_config && git checkout HEAD #{path_to_new_env}"
+            run "cd #{deploy_to}/tmp/env_config && git checkout HEAD #{app_name}/.env.#{current_stage}"
 
-            changed = run "diff #{path_to_new_env} #{path_to_new_env}"
-            if changed && /^y/i =~ Capistrano::CLI.ui.ask("Do you wish to override the existing .env file (y/n)? ")
+            lines_changed = capture("diff #{path_to_new_env} #{path_to_old_env} | wc -l").chomp.to_i
+
+            if lines_changed > 0 && /^y/i =~ Capistrano::CLI.ui.ask("Do you wish to override the existing .env file (y/n)? ")
               run "cp #{path_to_new_env}  #{path_to_old_env}"
             end
 
