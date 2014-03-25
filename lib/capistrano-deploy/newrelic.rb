500949ba49242c9c2c2d2486d37f733ec78ded65
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-03-25 15:35:15 -0600

    fix api key

--- lib/capistrano-deploy/newrelic.rb
+++ lib/capistrano-deploy/newrelic.rb
@@ -6,8 +6,7 @@ module CapistranoDeploy
         namespace :newrelic do
 
           task :notice_deployment do
-            #run "curl -H 'eecea6ff70c4296b9dc0d8ff1761c8354e4a41e9d559404:eecea6ff70c4296b9dc0d8ff1761c8354e4a41e9d559404' -d 'deployment[app_name]=Your Account' https://api.newrelic.com/deployments.xml"
-            run "curl -H 'eecea6ff70c4296b9dc0d8ff1761c8354e4a41e9d559404:eecea6ff70c4296b9dc0d8ff1761c8354e4a41e9d559404' -d 'deployment[application_id]=2936733' -d 'deployment[description]=This deployment was sent using curl' -d 'deployment[revision]=1242' -d 'deployment[changelog]=many hands make light work' -d 'deployment[user]=Ruben Estevez' https://api.newrelic.com/deployments.xml"
+            run "curl -H 'x-api-key:eecea6ff70c4296b9dc0d8ff1761c8354e4a41e9d559404' -d 'deployment[app_name]=Your Account' https://api.newrelic.com/deployments.xml"
           end
         end
 
