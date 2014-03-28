b8c58d5f457a1a2d85245fc82446b6f60778812a
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-03-28 11:28:20 -0600

    Change vairable names
    * change variable names for app_name
    and user for it to not conflict with
    the ones on capfile.

--- lib/capistrano-deploy/newrelic.rb
+++ lib/capistrano-deploy/newrelic.rb
@@ -5,15 +5,15 @@ module CapistranoDeploy
       configuration.load do
 
         set(:api_key) { ENV['NEW_RELIC_API_KEY'] }
-        set(:app_name) { ENV['NEW_RELIC_APP_NAME'] }
-        set(:user) { capture("users").chomp }
+        set(:new_relic_app_name) { ENV['NEW_RELIC_APP_NAME'] }
+        set(:new_relic_user) { capture("users").chomp }
         set(:current_revision) { capture("cd #{deploy_to} && git rev-parse HEAD").chomp }
         set(:link) { "https://api.newrelic.com/deployments.xml" }
 
         namespace :newrelic do
 
           task :notice_deployment do
-            run "curl -H '#{api_key}' -d 'deployment[app_name]=#{app_name}' -d 'deployment[revision]=#{current_revision}' -d 'deployment[user]=#{user}' #{link}"
+            run "curl -H '#{api_key}' -d 'deployment[app_name]=#{new_relic_app_name}' -d 'deployment[revision]=#{current_revision}' -d 'deployment[user]=#{new_relic_user}' #{link}"
           end
         end
 
