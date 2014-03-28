e44c958f6f54ac239ecf6810f7a43954930ad960
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-03-28 11:20:13 -0600

    Add user variable
    * Add user vairable using the current
    user logged in on terminal.
    * Change app name to user the env one
    instead of the one in cap file. the one
    in cap file differs from the one on new relic
    giving an error on deploy.

--- lib/capistrano-deploy/newrelic.rb
+++ lib/capistrano-deploy/newrelic.rb
@@ -4,14 +4,16 @@ module CapistranoDeploy
     def self.load_into(configuration)
       configuration.load do
 
-        set(:current_revision) { capture("cd #{deploy_to} && git rev-parse HEAD").chomp }
         set(:api_key) { ENV['NEW_RELIC_API_KEY'] }
+        set(:app_name) { ENV['NEW_RELIC_APP_NAME'] }
+        set(:user) { capture("users").chomp }
+        set(:current_revision) { capture("cd #{deploy_to} && git rev-parse HEAD").chomp }
         set(:link) { "https://api.newrelic.com/deployments.xml" }
 
         namespace :newrelic do
 
           task :notice_deployment do
-            run "curl -H '#{api_key}' -d 'deployment[app_name]=#{app_name}' -d 'deployment[revision]=#{current_revision}' #{link}"
+            run "curl -H '#{api_key}' -d 'deployment[app_name]=#{app_name}' -d 'deployment[revision]=#{current_revision}' -d 'deployment[user]=#{user}' #{link}"
           end
         end
 
