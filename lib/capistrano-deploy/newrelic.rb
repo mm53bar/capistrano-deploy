a5d7e68e38595c73a73a16ba8820fdd0ee23be43
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-03-28 10:45:32 -0600

    Add revision variable
    * Add revision variable. will grab the last
    commit id fron the log.
    * Move the api key to be an ENV variable.
    * Refactor notice deployment body to use variables
    instead of strings.

--- lib/capistrano-deploy/newrelic.rb
+++ lib/capistrano-deploy/newrelic.rb
@@ -3,10 +3,15 @@ module CapistranoDeploy
   module Newrelic
     def self.load_into(configuration)
       configuration.load do
+
+        set(:current_revision) { capture("cd #{deploy_to} && git rev-parse HEAD").chomp }
+        set(:api_key) { ENV['NEW_RELIC_API_KEY'] }
+        set(:link) { "https://api.newrelic.com/deployments.xml" }
+
         namespace :newrelic do
 
           task :notice_deployment do
-            run "curl -H 'x-api-key:eecea6ff70c4296b9dc0d8ff1761c8354e4a41e9d559404' -d 'deployment[app_name]=#{app_name}' https://api.newrelic.com/deployments.xml"
+            run "curl -H '#{api_key}' -d 'deployment[app_name]=#{app_name}' -d 'deployment[revision]=#{current_revision}' #{link}"
           end
         end
 
