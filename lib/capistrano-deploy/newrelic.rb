ab8a0593bbe24aea10a8abf8e06a426cf7295b0d
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-03-28 09:37:53 -0600

    Change app name
    * Change app name for a generic one,
    will capture the app name being deploy.

--- lib/capistrano-deploy/newrelic.rb
+++ lib/capistrano-deploy/newrelic.rb
@@ -6,7 +6,7 @@ module CapistranoDeploy
         namespace :newrelic do
 
           task :notice_deployment do
-            run "curl -H 'x-api-key:eecea6ff70c4296b9dc0d8ff1761c8354e4a41e9d559404' -d 'deployment[app_name]=Your Account' https://api.newrelic.com/deployments.xml"
+            run "curl -H 'x-api-key:eecea6ff70c4296b9dc0d8ff1761c8354e4a41e9d559404' -d 'deployment[app_name]=#{app_name}' https://api.newrelic.com/deployments.xml"
           end
         end
 
