d419a1caa43fbdc42d50b63a5b7a90bf78ce85e2
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-04-01 11:08:42 -0600

    Move new relic vairables outside the recipe
    * Move the app name and api key to be on the
    capfile.
    * Change the user to get the git user instead
    of the command line user.

--- lib/capistrano-deploy/newrelic.rb
+++ lib/capistrano-deploy/newrelic.rb
@@ -4,20 +4,17 @@ module CapistranoDeploy
     def self.load_into(configuration)
       configuration.load do
 
-        set(:api_key) { ENV['NEW_RELIC_API_KEY'] }
-        set(:new_relic_app_name) { ENV['NEW_RELIC_APP_NAME'] }
-        set(:new_relic_user) { (%x(users)).chomp }
+        set(:new_relic_user) { (%x(git config user.name)).chomp }
         set(:current_revision) { capture("cd #{deploy_to} && git rev-parse HEAD").chomp }
         set(:link) { "https://api.newrelic.com/deployments.xml" }
 
         namespace :newrelic do
 
           task :notice_deployment do
-            run "curl -H '#{api_key}' -d 'deployment[app_name]=#{new_relic_app_name}' -d 'deployment[revision]=#{current_revision}' -d 'deployment[user]=#{new_relic_user}' #{link}"
+            run "curl -H '#{new_relic_api_key}' -d 'deployment[app_name]=#{new_relic_app_name}' -d 'deployment[revision]=#{current_revision}' -d 'deployment[user]=#{new_relic_user}' #{link}"
           end
         end
 
-        after 'unicorn:reexec', 'newrelic:notice_deployment'
       end
     end
   end
