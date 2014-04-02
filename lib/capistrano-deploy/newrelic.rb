7febb61addd29f2159f90d126714d78388350cba
Ruben Estevez <ruben.a.estevez@gmail.com>
2014-04-02 14:15:19 -0600

    Add description to deployment hook
    * Add description variable that will
    hold the stage of the project on deploy.

--- lib/capistrano-deploy/newrelic.rb
+++ lib/capistrano-deploy/newrelic.rb
@@ -11,7 +11,9 @@ module CapistranoDeploy
         namespace :newrelic do
 
           task :notice_deployment do
-            run "curl -H '#{new_relic_api_key}' -d 'deployment[app_name]=#{new_relic_app_name}' -d 'deployment[revision]=#{current_revision}' -d 'deployment[user]=#{new_relic_user}' #{link}"
+            run "curl -H '#{new_relic_api_key}' -d 'deployment[app_name]=#{new_relic_app_name}'
+                -d 'deployment[revision]=#{current_revision}' -d 'deployment[user]=#{new_relic_user}'
+                -d deployment[description]='#{current_stage}' #{link}"
           end
         end
 
